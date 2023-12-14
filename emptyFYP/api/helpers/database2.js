270f158d8019f053fb7184a986994be8281d2656
Cally-0831 <19222068@life.hkbu.edu.hk>
2023-12-14 23:54:48 +0800

    push

--- emptyFYP/api/helpers/database2.js
+++ emptyFYP/api/helpers/database2.js
@@ -3,8 +3,8 @@ const { createPool } = require("mysql2")
 const pool = createPool({
     host: "fypdeploy-mysql",
     //host: "localhost",
-    //user: "root",
-    user:"fypdeploy",
+    user: "root",
+    //user:"fypdeploy",
     port:3306,
     password: "Psycho.K0831",
     database: "fypdeploy",
