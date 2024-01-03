c9df8b9aa03b0f8fbe51617857cbde22b0a0a756
Cally-0831 <19222068@life.hkbu.edu.hk>
2024-01-03 14:52:03 +0800

    push

--- SQL/Standard/PairingObserver.sql
+++ SQL/Standard/PairingObserver.sql
@@ -2,35 +2,35 @@
 -- Query: select * from observerpairstudent
 LIMIT 0, 1000
 
--- Date: 2024-01-02 22:34
+-- Date: 2024-01-03 12:32
 */
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00003','sid10000','Dr Amantha');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00002','sid11111','Prof Lam');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00007','sid12222','Dr Eva');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00006','sid10000','Dr dabbie');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00003','sid11111','Dr Amantha');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00009','sid12222','Dr George');
 INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00003','sid13333','Dr Amantha');
 INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00010','sid14444','Prof Harry');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00006','sid15555','Dr dabbie');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00008','sid20000','Prof Fanny');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00010','sid15555','Prof Harry');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00003','sid20000','Dr Amantha');
 INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00001','sid21111','Prof Chan');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00008','sid30000','Prof Fanny');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00004','sid31111','Dr Banana');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00007','sid32222','Dr Eva');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00002','sid33333','Prof Lam');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00004','sid34444','Dr Banana');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00010','sid35555','Prof Harry');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00009','sid40000','Dr George');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00008','sid41111','Prof Fanny');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00006','sid30000','Dr dabbie');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00001','sid31111','Prof Chan');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00008','sid32222','Prof Fanny');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00008','sid33333','Prof Fanny');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00001','sid34444','Prof Chan');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00004','sid35555','Dr Banana');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00005','sid40000','Prof Catie');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00005','sid41111','Prof Catie');
 INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00003','sid42222','Dr Amantha');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00005','sid43333','Prof Catie');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00005','sid44444','Prof Catie');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00008','sid50000','Prof Fanny');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00001','sid51111','Prof Chan');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00007','sid52222','Dr Eva');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00001','sid43333','Prof Chan');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00010','sid44444','Prof Harry');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00003','sid50000','Dr Amantha');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00008','sid51111','Prof Fanny');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00001','sid52222','Prof Chan');
 INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00001','sid53333','Prof Chan');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00001','sid60000','Prof Chan');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00005','sid61111','Prof Catie');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00003','sid62222','Dr Amantha');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00006','sid60000','Dr dabbie');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00006','sid61111','Dr dabbie');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00004','sid62222','Dr Banana');
 INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00004','sid70000','Dr Banana');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00003','sid71111','Dr Amantha');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00001','sid72222','Prof Chan');
-INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00001','sid73333','Prof Chan');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00006','sid71111','Dr dabbie');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00005','sid72222','Prof Catie');
+INSERT INTO observerpairstudent (`OID`,`SID`,`obsname`) VALUES ('tid00004','sid73333','Dr Banana');
